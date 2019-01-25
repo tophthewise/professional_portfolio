3e31978019f7e5f824eee9c7a69bdf58b8dd9eaa
tophthewise <eric.porter820@gmail.com>
2019-01-25 16:24:52 -0500

    finishing up last updates

--- src/Pages/CheckinIn/CheckinIn.js
+++ src/Pages/CheckinIn/CheckinIn.js
@@ -4,6 +4,7 @@ import {BrowserRouter} from 'react-router-dom';
 //import './Designer.css';
 import Projects from '../../Components/Projects/Projects';
 import Papers from '../../Components/Papers/Papers';
+import poster from '../../img/CheckinInPoster.png';
 
 
 
@@ -13,14 +14,30 @@ class CheckinIn extends Component{
 
 render(){
 
-
-
-
+const headerstyle={'padding-left':"2%"};
+const textStyle ={'padding-left':'2%'};
+const imgStyle ={'height':'100%','width':'40%', 'border-radius':'0px','border':'white solid','padding-bottom':'0px'}
+const title = {'padding-bottom':'1px'}
 
 return(
 <Fragment>
-{/*<p> Hi im paul!</p>*/}
+	<h1 className="center" style={title}>
+		{"Checkin' In"}
+	</h1>
 
+	<img className="center" style = {imgStyle} src={poster} />
+	<h2 style={headerstyle}>
+		Design
+	</h2>
+	<p style={textStyle}>
+		lorem ipsum
+	</p>
+	<h2 style={headerstyle}>
+		Process
+	</h2>
+	<p style={textStyle}>
+		lorem ipsum
+	</p>
 </Fragment>
 	);
 
--- src/Pages/EscapeKey/EscapeKey.js
+++ src/Pages/EscapeKey/EscapeKey.js
@@ -4,6 +4,7 @@ import {BrowserRouter} from 'react-router-dom';
 //import './Designer.css';
 import Projects from '../../Components/Projects/Projects';
 import Papers from '../../Components/Papers/Papers';
+import poster from '../../img/EscapeKeyPoster.png';
 
 
 
@@ -14,13 +15,29 @@ class EscapeKey extends Component{
 render(){
 
 
-
+const headerstyle={'padding-left':"2%"};
+const textStyle ={'padding-left':'2%'};
 
 
 return(
 <Fragment>
-{/*<p> Hi im paul!</p>*/}
+<h1 className="center">
+		{"Escape Key"}
+	</h1>
 
+	<img className="center" src={poster} />
+	<h2 style={headerstyle}>
+		Design
+	</h2>
+	<p style={textStyle}>
+		lorem ipsum
+	</p>
+	<h2 style={headerstyle}>
+		Process
+	</h2>
+	<p style={textStyle}>
+		lorem ipsum
+	</p>
 </Fragment>
 	);
 
