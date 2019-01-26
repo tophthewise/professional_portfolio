9263596609f644b0ce015c2626540b57b002b62a
tophthewise <eric.porter820@gmail.com>
2019-01-26 15:57:05 -0500

    additions to Escape Key page

--- src/Pages/EscapeKey/EscapeKey.js
+++ src/Pages/EscapeKey/EscapeKey.js
@@ -31,13 +31,27 @@ return(
 		Design
 	</h2>
 	<p style={textStyle}>
-		lorem ipsum
+	 Escape Key is a design for women who don't feel safe on college campuses to have a way out of dangerous situations. 
+	 The main ways to avoid dangerous situations are: escape routes, 911 quick dialing, or send a distress message.
 	</p>
 	<h2 style={headerstyle}>
 		Process
 	</h2>
 	<p style={textStyle}>
-		lorem ipsum
+	 First we chose a problem space that was important to college life in general which was safety, specifically safety for women on college campuses.
+		After defining the problem space we conducted a literature review and ideated solutions of problems that exist for women on college campuses.
+		In the ideation we found that there were a few common themes that were hazardous for women on college campuses. These issues were:
+		<ol>
+			<li>Navigating out of dangerous situations </li>
+			<li>Contacting a close friend when in trouble </li>
+			<li>Discretely contacting the authorities  </li>
+		</ol>
+		After the ideation session we conducted interviews women on college capmuses to cross reference our secondary data from our literature review.
+		Using the interviews we ideated solutions to solve the major safety concerns. 
+		We came out with a low fidelity protoype that addressed all three concerns of having a discrete way to contact the authorities and friends with a location, and navigating to a safe area when in trouble. 
+		Then we used  the low fidelity to run user tests to the same users from the interviews as a focal point to face the safety concerns and user experience. 
+		The user tests guided the team to implement changes from the low fidelity prototype in creating our high fideility prototype. 
+		The final design was sumarized in the poster above.  
 	</p>
 </Fragment>
 	);
