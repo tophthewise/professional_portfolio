110493d0305f4e6cefac97e01249c255b49e21ad
tophthewise <eric.porter820@gmail.com>
2019-03-11 14:37:31 -0400

    responsive web changes

--- src/Components/Designer/Designer.js
+++ src/Components/Designer/Designer.js
@@ -1,5 +1,6 @@
-import React,{Component} from 'react';
-import backdrop from '../../img/Design_drop3.jpg';
+import React,{Component,Fragment} from 'react';
+import backdrop from '../../img/Design_drop_4.jpeg';
+import backdrop_m from '../../img/Design_drop3.jpg';
 //change image to something dope 
 import {Link} from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -7,10 +8,18 @@ import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Designer.css';
+import MediaQuery from 'react-responsive';
+
 library.add(fab,fas);
 
 const myStyle = {
 	backgroundImage:`url( ${backdrop} )`,
+	height:500,
+	backgroundSize:'cover',
+
+} 
+const myStyle2 = {
+	backgroundImage:`url( ${backdrop_m} )`,
 	height:500,
 	backgroundSize:'cover',
 
@@ -22,6 +31,8 @@ class Designer extends Component{
 
 render(){
 return(
+<Fragment>
+<MediaQuery minWidth = {1000}>
 <div className="tile" >
 <header  style={myStyle}> 
 <h1 > {this.props.title}</h1>
@@ -31,6 +42,19 @@ return(
 </Link> </p>
 </header>
 </div>
+</MediaQuery>
+<MediaQuery maxWidth = {1000}>
+<div className="tile" >
+<header  style={myStyle2}> 
+<h1 > {this.props.title}</h1>
+<p > <Link to="/Designer" style={subhead}>
+{" Designs " }
+<FontAwesomeIcon icon={['fas','drafting-compass']} />
+</Link> </p>
+</header>
+</div>
+</MediaQuery>
+</Fragment>
 );
 
 	}
