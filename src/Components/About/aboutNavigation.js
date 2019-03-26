803aca3d54f8e47b40b95c9756b855b6cf8ea09b
tophthewise <eric.porter820@gmail.com>
2019-03-26 17:02:04 -0400

    Resume Update

--- src/Components/About/aboutNavigation.js
+++ src/Components/About/aboutNavigation.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import '../Navigation/Navigation.css';
-import Resume from '../../Files/Resume_Eric_Porter_12_18_2018.pdf';
+import Resume from '../../Files/Resume_Eric_Porter_3_25_2019.pdf';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
